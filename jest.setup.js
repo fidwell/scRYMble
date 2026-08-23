1c1670ad49c70d000293b074bd35aa81ce227863
Andrew <5436387+fidwell@users.noreply.github.com>
2026-08-22 21:00:09 -0400

    Fix jest setup, package.json mistakes

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,7 @@ global.GM_getValue = jest.fn().mockImplementation((key, defaultValue) => {
 
 global.GM_setValue = jest.fn().mockImplementation((key, value) => {
 });
+
+global.GM_deleteValue = jest.fn();
+
+global.GM_xmlhttpRequest = jest.fn();
