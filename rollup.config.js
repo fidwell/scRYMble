5e3d81d94ca46bb9a01b71c36730dfad103a934a
Andrew <5436387+fidwell@users.noreply.github.com>
2026-08-22 21:47:38 -0400

    Fix banner transform, maybe

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import replace from "@rollup/plugin-replace";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { scRYMbleBanner } from "./meta/scRYMbleBanner.js";
 import { getVersion } from "./meta/version.js";
 
 function userscriptBanner() {
+  const banner = scRYMbleBanner.replace("__buildDate__", getVersion());
   return {
     name: "userscript-banner",
     generateBundle(_options, bundle) {
       for (const file of Object.values(bundle)) {
         if (file.type === "chunk") {
-          file.code = `${scRYMbleBanner}\n${file.code}`;
+          file.code = `${banner}\n${file.code}`;
         }
       }
     }
@@ -20,11 +20,7 @@ function userscriptBanner() {
 export default {
   input: "src/scRYMble.ts",
   plugins: [
-    typescript({ tsconfig: "./tsconfig.json" }),
-    replace({
-      preventAssignment: true,
-      __buildDate__: getVersion
-    })
+    typescript({ tsconfig: "./tsconfig.json" })
   ],
   output: [
     {
